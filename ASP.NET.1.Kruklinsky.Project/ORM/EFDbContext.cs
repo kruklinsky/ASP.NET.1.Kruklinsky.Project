e7172b46a31534fb154255bb14f90bba1b8c274e
kruklinsky <kruklinsky.alexander@gmail.com>
2015-01-07 19:05:33 +0400

    Update ORM
    Add ORM Entities

---
+++ ASP.NET.1.Kruklinsky.Project/ORM/EFDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ORM.Model;
+using System.Data.Entity;
+
+namespace ORM
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("EFDbConnection") { }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasRequired(u => u.Profile).WithRequiredPrincipal(p => p.User).Map(m => m.MapKey("UserId"));
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Roles)
+                .WithMany(r => r.Users)
+                .Map(m => { m.ToTable("UsersInRoles"); m.MapLeftKey("UserId"); m.MapRightKey("RoleId"); });
+        }
+    }
+}
---
+++ ASP.NET.1.Kruklinsky.Project/ORM/Model/Membership/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ORM.Model
+{
+    public class Profile
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int ProfileId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}

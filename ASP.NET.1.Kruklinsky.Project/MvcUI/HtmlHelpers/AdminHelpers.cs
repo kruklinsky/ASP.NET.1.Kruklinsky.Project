5ec6a8ee3e27a0554ee3c7f8e1cbd7e04df0743f
kruklinsky <kruklinsky.alexander@gmail.com>
2015-01-18 15:53:10 +0300

    Add Html helpers
    Admin helpers
    Layout helpers

---
+++ ASP.NET.1.Kruklinsky.Project/MvcUI/HtmlHelpers/AdminHelpers.cs
@@ -0,0 +1,66 @@
+﻿using MvcUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MvcUI.HtmlHelpers
+{
+    public static class AdminHelpers
+    {
+        public static MvcHtmlString TestQuestions(this HtmlHelper html, IEnumerable<Question> questions, int hideId, string label)
+        {
+            return MvcHtmlString.Create(Hider(label, hideId, DisplayForQuestions(html, questions)));
+        }
+
+        private static string DisplayForQuestions(HtmlHelper html, IEnumerable<Question> questions)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var question in questions)
+            {
+                result.Append(html.ActionLink(question.Text.Substring(0,50)+"...", "EditQuestion", "Admin", new { questionId = question.Id }, null));
+                result.Append("<br /> <br />");
+            }
+            return result.ToString();
+        }
+
+        #region Hider
+
+        public static string Hider(string label, int hideId, string innerHtml)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append(Image(hideId));
+            result.Append(Label(label));
+            result.Append(InnerHtml(hideId, innerHtml));
+            return result.ToString();
+        }
+
+        public static string Image(int hideId)
+        {
+            TagBuilder result = new TagBuilder("img");
+            result.MergeAttribute("onclick", "hide(" + hideId + ")&changeImage(jQuery.event.fix(event).target)");
+            result.MergeAttribute("src", "/Content/admin/images/hide.png");
+            result.MergeAttribute("width", "15");
+            result.MergeAttribute("height", "15");
+            return result.ToString();
+        }
+        public static string Label(string label)
+        {
+            TagBuilder result = new TagBuilder("label");
+            result.InnerHtml = label;
+            return result.ToString();
+        }
+        public static string InnerHtml (int hideId, string innerHtml)
+        {
+            TagBuilder result = new TagBuilder("div");
+            result.MergeAttribute("id", hideId.ToString());
+            result.InnerHtml = innerHtml;
+            return result.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ ASP.NET.1.Kruklinsky.Project/MvcUI/HtmlHelpers/LayoutHelpers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MvcUI.HtmlHelpers
+{
+    public static class LayoutHelpers
+    {
+        #region LogButtons
+
+        public static MvcHtmlString LogButtons(this HtmlHelper html, bool isAuthenticated, bool isAdmin, string actionName)
+        {
+            StringBuilder result = new StringBuilder();
+            if (isAuthenticated)
+            {
+                AuthenticatedButtons(html, isAdmin, ref result);
+            }
+            else
+            {
+                NoAuthenticatedButtons(html, actionName, ref result);
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+        private static void AuthenticatedButtons(HtmlHelper html, bool isAdmin, ref StringBuilder result)
+        {
+            RolesButtons(html, isAdmin, result);
+            result.Append(html.RouteLink("Log Out", new { controller = "Account", action = "LogOut" }, new { @id = "Button" }));
+        }
+
+        private static void NoAuthenticatedButtons(HtmlHelper html, string actionName, ref StringBuilder result)
+        {
+            if (actionName != "SignUp")
+            {
+                result.Append(html.RouteLink("Sing Up", new { controller = "Account", action = "SignUp" }, new { @id = "Button" }));
+            }
+            else
+            {
+                result.Append(html.RouteLink("Log In", new { controller = "Account", action = "LogIn" }, new { @id = "Button" }));
+            }
+        }
+
+        private static void RolesButtons(HtmlHelper html, bool isAdmin, StringBuilder result)
+        {
+            if (isAdmin)
+            {
+                result.Append(html.RouteLink("Administration | ", new { controller = "Admin", action = "Index" }, new { @id = "Button" }));
+            }
+            else
+            {
+                result.Append(html.RouteLink("Profile | ", new { controller = "Profile", action = "Index" }, new { @id = "Button" }));
+            }
+        }
+
+        #endregion
+    }
+}
